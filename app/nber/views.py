a0bf374c231d26d2fea9a915bfa9cc7d78a36b0d
xinyuan8D <xinyuan.chn@gmail.com>
2018-08-04 14:12:39 -0700

    add fake uuidlist, signin/signup endpoint

--- app/nber/views.py
+++ app/nber/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template as view
+from flask import request
 
 nber = Blueprint('nber', __name__, static_folder='static', template_folder='templates')
-
+uuidlist = {'Lanchao', 'Seven'}
 
 @nber.route('/nber')
 def index():
@@ -10,3 +11,13 @@ def index():
     :return:
     """
     return view('nber/index.html')
+
+
+@nber.route('/nber/post_auth')
+def post_auth():
+    # http://identityChain.com/nber/post_auth?uuid=[uuid]
+    uuid = request.args.get('uuid')
+    if uuid in uuidlist: 
+        return view('/nber/signin.html')
+    else:
+        return view('/nber/signup.html')
